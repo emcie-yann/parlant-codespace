03b71c56df398711e0479d03328f2d439259222a
emcie-yann <yann@emcie.co>
2025-11-26 20:26:27 +0000

    added changes

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import asyncio
 import parlant.sdk as p
+from dotenv import load_dotenv
+
+load_dotenv()
 
 async def main():
-  async with p.Server() as server:
+  async with p.Server(
+    session_store='local',
+    port=8800,
+    migrate=True,
+    log_level=p.LogLevel.TRACE,
+  ) as server:
     agent = await server.create_agent(
         name="Otto Carmen",
         description="You work at a car dealership",
